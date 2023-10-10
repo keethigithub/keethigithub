9f3d545bc85d30c540e0551a799e318897c7df64
keethigithub <147532099+keethigithub@users.noreply.github.com>
2023-10-10 21:22:56 +0530

    Create o.s

---
+++ o.s
@@ -0,0 +1,68 @@
+1. File and Directory Operations:
+
+cd (Change Directory):
+
+Usage: cd directory_name or cd /path/to/directory
+Example: cd Documents
+pwd (Print Working Directory):
+
+Usage: pwd
+Example: pwd
+ls (List Files and Directories):
+
+Usage: ls or ls directory_name
+Example: ls or ls Documents
+mkdir (Make Directory):
+
+Usage: mkdir directory_name
+Example: mkdir NewFolder
+touch (Create Empty File):
+
+Usage: touch file_name
+Example: touch myfile.txt
+rm (Remove/Delete):
+
+Usage: rm file_name (for files) or rm -r directory_name (for directories)
+Example: rm myfile.txt or rm -r OldFolder
+2. File Manipulation:
+
+cp (Copy):
+
+Usage: cp source destination
+Example: cp file.txt newfile.txt
+mv (Move/Rename):
+
+Usage: mv source destination (for moving) or mv old_name new_name (for renaming)
+Example: mv file.txt /path/to/destination or mv oldfile.txt newfile.txt
+cat (Concatenate and Display File Content):
+
+Usage: cat file_name
+Example: cat myfile.txt
+more or less (View File Content Page by Page):
+
+Usage: more file_name or less file_name
+Example: more longfile.txt
+3. System Information:
+
+uname (Print System Information):
+
+Usage: uname -a
+Example: uname -a
+df (Disk Free - Display Disk Space Usage):
+
+Usage: df -h
+Example: df -h
+top (Display System Processes):
+
+Usage: top
+Example: top
+4. Text Editing:
+
+nano (Text Editor):
+
+Usage: nano file_name
+Example: nano myfile.txt
+vim or emacs (Advanced Text Editors):
+
+Usage: vim file_name or emacs file_name
+Example: vim myfile.txt
